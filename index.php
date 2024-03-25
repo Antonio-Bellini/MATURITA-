ce1a14f51cbd7cf8980717cd2b75922f94d067cd
Ant00o <belliniantonio205@gmail.com>
2024-03-25 18:07:58 +0100

    iniziato lo stile della pagina

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
-    <title>Associazione ZeroTre</title>
+    <script src="script/script.js"></script>
+    <link rel="icon" href="style/loghi/logo.png" type="x-icon">
+    <title>ZeroTre</title>
 </head>
 <!-- STAMPA del BODY in BASE al COOKIE SALVATO -->
 <?php
@@ -14,13 +17,31 @@
 ?>
     <!-- SEZIONE PRINCIPALE della PAGINA INDEX -->
     <main>
-        <?php
-            echo "  <button><a href='index.php'>HOME</a></button>
-                    <button><a href='newsletter.php'>NEWSLETTER</a></button>
-                    <button><a href='bacheca.php'>BACHECA</a></button>
-                    <button><a href='private/area_personale.php'>AREA PERSONALE</a></button>";
-        ?>
-        <h1>HOME PAGE DEL SITO WEB</h1>
+        <section class="header">
+            <nav>
+                <a href='index.php'>
+                    <img 
+                        src="style/loghi/logo.png"
+                        class="logo"
+                        id="logoImg"
+                        alt="logo associazione"
+                    />
+                </a>
+                <div class="nav_links" id="navLinks">
+                    <ul>
+                        <li><a href="newsletter.php"             class="btn">Newsletter   </a></li>
+                        <li><a href="bacheca.php"                class="btn">Bacheca       </a></li>
+                        <li><a href="https://stripe.com/it"      class="btn">Donazioni     </a></li>
+                        <li><a href="private/area_personale.php" class="btn">Area Personale</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <!--Central section-->
+            <div class="text">
+                <h1>ZeroTre</h1>
+            </div>
+        </section>
     </main>
 </body>
 </html>
