81025426cfa6f41c2f4e7caa1a072354a7007848
Ant00o <belliniantonio205@gmail.com>
2024-03-26 18:08:23 +0100

    footer

--- index.php
+++ index.php
@@ -47,5 +47,18 @@
 
         <div class="image-slider"></div>
     </main>
+
+    <footer>
+        <h4>Dove siamo</h4>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d838.2236730786605!2d7.650056144002364!3d45.036350637386306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478812d12cec3a6f%3A0xc06c1b91d782d7d5!2sCorso%20Unione%20Sovietica%2C%20220d%2C%2010134%20Torino%20TO!5e0!3m2!1sit!2sit!4v1711472330414!5m2!1sit!2sit" 
+                width="300" height="250" 
+                style="border-radius: 10px; border:none;" 
+                allowfullscreen="" 
+                loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+        <p>Associazione ZeroTre ODV</p>
+        <p>Corso Unione Sovietica 220/D, 10126, Torino (TO)</p>
+    </footer>
 </body>
 </html>
