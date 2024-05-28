0ad63ce96f6c0b7a716aae45b43ed020b3273b07
Ant00o <belliniantonio205@gmail.com>
2024-05-28 21:09:37 +0200

    aggiunto servizio di invio email direttamente dal sito

--- volunteer_request.php
+++ volunteer_request.php
@@ -7,6 +7,8 @@
     echo "<script src='https://code.jquery.com/jquery-3.6.4.min.js'></script>";
     echo WEBALL;
     echo "<script src='script/script.js'></script>";
+    echo "<script type='text/javascript'src='https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js'></script>";
+    echo "<script type='text/javascript'> emailjs.init('cx_DHiqEbFqDomUtu') </script>";
     echo "<link rel='stylesheet' href='style/style.css'>";
     echo "<title>Associazione Zero Tre</title>";
     importActualStyle();
@@ -48,9 +50,32 @@
                 <label for='info'>Parlaci un po' di te</label>
                 <textarea name='info' id='info' cols='30' rows='10' placeholder='Dicci qualcosa su di te'></textarea>
 
-                <input type='submit' value='INVIA'>
+                <input type='submit' value='INVIA' id='send_form'>
             </form>
         </section>";
+
+    // js per invio dell'email tramite utilizzo di WS fornito da EmailJS
+    echo "  <script>
+                const btn = document.getElementById('send_form');
+
+                document.getElementById('volunteer_request').addEventListener('submit', function (e) {
+                    e.preventDefault();
+
+                    btn.value = 'Invio...';
+
+                    const serviceID = 'default_service';
+                    const templateID = 'template_fuonb84';
+
+                    emailjs.sendForm(serviceID, templateID, this)
+                        .then(() => {
+                            btn.value = 'Inviato';
+                            alert('Email inviata correttamente, ti ricontatteremo presto!');
+                        }, (err) => {
+                            btn.value = 'Errore';
+                            alert(JSON.stringify(err));
+                        });
+                });
+            </script>";
 
     show_footer();
 ?>
